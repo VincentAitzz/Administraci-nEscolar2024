963f888685bfdf929f352485ac3df943b2c20e87
VincentAitzz <v.andrehpave@gmail.com>
2024-05-06 13:28:55 -0400

    Agregar Index y su logica

--- index.php
+++ index.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    
+    <div class="input-field">
+
+        <img src="IMG/logoUsuario.png" class="logoUsuario">
+        <img src="IMG/icons8-bloquear-64.png" class="logoContraseña">
+
+        <form action="actionsPhp/logicaLogin.php" method="post">
+            <input type="text" class="inputUsu" required name="usuario">
+            <label class="labelUsu">Usuario</label>
+            <br>
+            <input type="password" class="inputPas" required name="contraseña">
+            <label class="labelPas">Contraseña</label>
+            <br>
+            <label class="labelLink"><a href="#" class="Link">¿Ha olvidado su Contraseña?</a></label>
+            <br>
+            <input type="submit" value="Login" class="btn -btn1">
+        </form>
+    </div>
 </body>
 </html>
