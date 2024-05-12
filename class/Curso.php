3ec7fe34a5e2e5583ae7377ac81445ea769e35fa
RKDJ12 <aweonaoxd2004@gmail.com>
2024-05-12 19:00:47 -0400

    19:00
    Buscador como el pico, la tabla iwal, help

--- class/Curso.php
+++ class/Curso.php
@@ -4,10 +4,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administración | Curso</title>
+    <link rel="stylesheet" href="../css/StyleCurso.css">
 </head>
 <body>
-Estas en Cursos
-    <br>
-    <a href="../HUB.php">Volver</a>
+    <div class="container">
+        <h2>Administración de Curso</h2>
+        <div class="input-field">
+            <!-- ID CURSO PARA ACTUALIZAR/ELIMINAR-->
+            <input type="text" id="IDCurso" class="inputIDCurso" required>
+            <label for="IDCurso" class="lblIDCurso">ID Curso</label>
+            <br>
+            <!--Numero de alumnos Curso-->
+            <input type ="text" id="NumeroAlumnos" class="inputNumAlumnos" required>
+            <label for="NumeroAlumnos" class="lblNumeroAlumnos">Numero de Alumnos</label>
+            <!--Combo box nivel-->
+            <select id="nivel" name="nivel" required>
+            <option value="">Seleccione nivel...</option>
+            <?php include '../actionsPhp/cargarNivel.php';?>
+            </select>
+            <br>
+            <!--Letra Curso-->
+            <select id="Letra" name="letra">
+                <option value="">Seleccione letra...</option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+                <option value="E">E</option>
+                <option value="F">F</option>
+                <option value="G">G</option>
+                
+            </select>
+            <br>
+            
+        <!--Termina el container-->
+        <!--Botones-->
+            <div class="buttons">
+                <button class="btnCRUDD" id="btnAgregar">Agregar</button>
+                <button class="btnCRUDD" id="btnEditar">Editar</button>
+                <button class="btnCRUDD" id="btnEliminar">Eliminar</button>
+                <br>
+            </div>
+        </div>
+    </div>
+    <div class="input-field2">
+        <input type="text" class="inputBuscar" required>
+        <label for="inputBuscar" class="lblBuscar">Buscador</label>
+        <br> 
+    </div>
+    <!-- Tabla -->   
+    <div class="tabla">
+        <table id="tabla-curso">
+            <tr>
+                <th>ID</th>
+                <th>Nivel</th>
+                <th>Letra</th>
+                <th>Numero Alumnos</th>
+            </tr>
+        </table>
+    <!--Volver boton-->
+    <div class="btnVolver">
+        <a href="../HUB.php" class="btn -btnVolver">Volver</a>
+    </div>
 </body>
 </html>
